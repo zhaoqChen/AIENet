472922e6a1124bee73aaac92aa60ad961060d02b
zhaoqChen <112755265+zhaoqChen@users.noreply.github.com>
2023-01-30 02:53:53 +0800

    Create FolderLoader.py

---
+++ FolderLoader.py
@@ -0,0 +1,20 @@
+import io
+import os
+import utils
+import struct
+from PIL import Image
+
+class FolderLoader(object):
+    def __init__(self, fold_path):
+        super(FolderLoader, self).__init__()
+        self.fold_path = fold_path
+        self.img_paths = utils.make_dataset(self.fold_path)
+        self.img_names = [os.path.basename(x) for x in self.img_paths]
+
+    def __getitem__(self, index):
+        img = Image.open(self.img_paths[index])#.convert('RGB')
+        if img.width > 1000:
+            img = img.resize((img.width//2,img.height//2), Image.BILINEAR)
+        return self.img_names[index], img
+    def __len__(self):
+        return len(self.img_names)
